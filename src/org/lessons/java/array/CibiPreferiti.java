b46c99118ec164185283e21003ca64d3f64e7c58
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-13 17:38:24 +0200

    fix typo

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class CibiPreferiti {
 	public static void main(String[] args) {
-		String[] favoriteFoods = {"Pizza", "Pasta", "Hamburger", "Sushi", "Bistecca", "Patatine Fritte", "Prova"};
+		String[] favoriteFoods = {"Pizza", "Pasta", "Hamburger", "Sushi", "Bistecca", "Patatine Fritte"};
 		
 		System.out.println("Your have " + favoriteFoods.length + " favorite foods");
 		System.out.println("Your favorite food is " + favoriteFoods[0]);
@@ -13,15 +13,15 @@ public class CibiPreferiti {
 		
 		//even scenario
 		if(favoriteFoods.length %2 == 0) {
-			//select the food with the index == to half of favoriteFoods.lenght and = to half of favoriteFood.lenght - 1
+			//select the foods with the index == to half of favoriteFoods.lenght and = to half of favoriteFood.lenght - 1
 			String[] middleRankFoodsEven = {favoriteFoods[(favoriteFoods.length / 2) - 1], favoriteFoods[favoriteFoods.length / 2]};
 			System.out.println("Your average favorite food are " + Arrays.toString(middleRankFoodsEven));
 		//odd scenario
 		} else {
 			//select the food with the index == to half of the favoriteFood.lenght rounded up 
-			String middleRankOdd = favoriteFoods[favoriteFoods.length / 2];
+			String middleRankFoodOdd = favoriteFoods[favoriteFoods.length / 2];
 			System.out.println(favoriteFoods.length / 2);
-			System.out.println("Your average favorite food is " + middleRankOdd);
+			System.out.println("Your average favorite food is " + middleRankFoodOdd);
 		}
 	}
 }
