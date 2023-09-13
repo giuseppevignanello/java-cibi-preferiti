6ddd191042397c562b7da563993b053bce23492a
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-13 17:11:32 +0200

    print length, first and last

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -2,6 +2,10 @@ package org.lessons.java.array;
 
 public class CibiPreferiti {
 	public static void main(String[] args) {
+		String[] favoriteFoods = {"Pizza", "Pasta", "Hamburger", "Sushi", "Bistecca", "Patatine Fritte"};
 		
+		System.out.println(favoriteFoods.length);
+		System.out.println(favoriteFoods[0]);
+		System.out.println(favoriteFoods[favoriteFoods.length - 1]);
 	}
 }
