7527c95b3f9ef25d67a8d0368b49355f4f33ed09
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-13 17:44:39 +0200

    refactoring odd and even scenarios

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -5,23 +5,22 @@ import java.util.Arrays;
 
 public class CibiPreferiti {
 	public static void main(String[] args) {
-		String[] favoriteFoods = {"Pizza", "Pasta", "Hamburger", "Sushi", "Bistecca", "Patatine Fritte"};
+		String[] favoriteFoods = {"Pizza", "Pasta", "Hamburger", "Sushi", "Bistecca", "Patatine Fritte", "Prova"};
 		
 		System.out.println("Your have " + favoriteFoods.length + " favorite foods");
 		System.out.println("Your favorite food is " + favoriteFoods[0]);
 		System.out.println("Your less favorite food is " + favoriteFoods[favoriteFoods.length - 1]);
 		
+		String middleRankFood = favoriteFoods[favoriteFoods.length / 2];
+		
 		//even scenario
 		if(favoriteFoods.length %2 == 0) {
-			//select the foods with the index == to half of favoriteFoods.lenght and = to half of favoriteFood.lenght - 1
-			String[] middleRankFoodsEven = {favoriteFoods[(favoriteFoods.length / 2) - 1], favoriteFoods[favoriteFoods.length / 2]};
-			System.out.println("Your average favorite food are " + Arrays.toString(middleRankFoodsEven));
+			String anotherMiddleRankFood = favoriteFoods[(favoriteFoods.length / 2) - 1];
+			System.out.println("Your average favorite food are " + anotherMiddleRankFood + " and " + middleRankFood);
 		//odd scenario
 		} else {
-			//select the food with the index == to half of the favoriteFood.lenght rounded up 
-			String middleRankFoodOdd = favoriteFoods[favoriteFoods.length / 2];
-			System.out.println(favoriteFoods.length / 2);
-			System.out.println("Your average favorite food is " + middleRankFoodOdd);
+			
+			System.out.println("Your average favorite food is " + middleRankFood);
 		}
 	}
 }
